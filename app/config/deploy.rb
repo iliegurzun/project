e3e28574c4671f03a89eb96e8fb1c99251ef9472
Ilie Gurzun <ilie.gurzun@emag.ro>
2015-10-22 16:05:36 +0300

    asd

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -19,7 +19,7 @@ ssh_options[:forward_agent] = true
 #ssh_options[:use_agent] = false
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
 
-set :domain,      "127.0.0.1"
+set :domain,      "127.0.0.1:3023"
 set :branch, 	  "master"
 
 set   :scm,               :git
@@ -52,7 +52,7 @@ set   :update_vendors,    false
 
 logger.level = Logger::MAX_LEVEL
 
-after 'deploy:updated',   'deploy:phpunit'
+after 'symfony:cache:warmup',   'deploy:phpunit'
 
 # permissions
 set :writable_dirs, ["app/cache", "app/logs"]
