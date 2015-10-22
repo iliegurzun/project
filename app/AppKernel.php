a513569cda6b6e88c92e6ab2fa73838f7e5a8971
Ilie Gurzun <ilie.gurzun@emag.ro>
2015-10-22 15:12:59 +0300

    testing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,9 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JDare\ClankBundle\JDareClankBundle(),
-            new Cunningsoft\ChatBundle\CunningsoftChatBundle(),
-            new Knp\Bundle\TimeBundle\KnpTimeBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         );
