9515ca8f9ef37119194051d1f3956426b7bc0bbe
Ilie Gurzun <ilie.gurzun@emag.ro>
2015-10-23 10:28:44 +0300

    123

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -62,7 +62,7 @@ set :use_set_permissions, false
 
 namespace :deploy do
 	task :phpunit, :roles => :app do
-		run "cd #{release_path} && php phpunit.phar --verbose --debug -c app/ --coverage-clover=#{release_path}/web/codeCoverage/clover.xml"
+		run "cd #{release_path} && php phpunit.phar --verbose --debug -c app/ --coverage-clover=#{release_path}/clover/clover.xml"
 		run "cd #{release_path} && ant"
 	end
 end
